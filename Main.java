033bf31fedebc4e212fa310671b6072fc57d0848
Krisz9000 <149705145+Krisz9000@users.noreply.github.com>
2025-04-04 10:43:59 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Violin violin = new Violin(1, "Stradivari", "Santa", 100, false);
+        violin.violinTuning();
+        violin.work();
+    }
+}
---
+++ Violin.java
@@ -0,0 +1,24 @@
+public class Violin extends MusicalInstrument {
+    String owner;
+    int age;
+    boolean isTuned;
+
+    public Violin(int weight, String tradeMark, String owner, int age, boolean isTuned) {
+        super(weight, tradeMark);
+        this.owner = owner;
+        this.age = age;
+        this.isTuned = isTuned;
+    }
+
+    @Override
+    public void work() {
+        System.out.println("Die Geige ertönt.");
+    }
+
+    public void violinTuning () {
+        System.out.println("Ich stimme die erste Saite...");
+        System.out.println("Ich stimme die zweite Saite...");
+        System.out.println("Ich stimme die dritte Saite...");
+        System.out.println("Ich stimme die vierte Saite...");
+    }
+}
